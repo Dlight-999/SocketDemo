8e99db8d3297b95852c9994beeb31261e01901dd
unknown <dracokd19@gmail.com>
2024-06-16 07:13:30 +0545

    fixes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 server.listen(5000, () => {
   console.log("Server is Running");
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { io, Socket } from "socket.io-client";
+const socket: Socket = io("http//localhost:5000");
 function App() {
+  const [message, setMessage] = useState<string>("");
+  const handlePress = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ): void => {
+    if (event.key === "Enter") {
+      if (event.shiftKey) {
+      } else {
+        event.preventDefault();
+        sendMessage();
+      }
+    }
+  };
+  const sendMessage = () => {
+    if (message) {
+      socket.emit("chat message", message);
+      setMessage("");
+    }
+  };
   return (
     <>
       <div className=" bg-slate-900 text-white h-screen flex flex-col items-center justify-between">
@@ -9,10 +28,21 @@ function App() {
         </div>
         <div className="func flex justify-between w-full">
           <div className="input w-full">
-            <input type="text" name="msg" id="msg" className="w-full h-full" />
+            <textarea
+              name="msg"
+              id="msg"
+              value={message}
+              onKeyDown={handlePress}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full h-full p-2 text-black flex"
+              style={{ resize: "none" }}
+              placeholder="Type a message..."
+            ></textarea>
           </div>
           <div className="btn">
-            <button className="h-full w-full p-3">Send</button>
+            <button className="h-full w-full p-3" onClick={sendMessage}>
+              Send
+            </button>
           </div>
         </div>
       </div>
